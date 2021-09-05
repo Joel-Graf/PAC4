a4cb4a3ec99b94680ecfb44259523ef46e6e7139
Joel Marcos Graf <55204995+Joel-Graf@users.noreply.github.com>
2021-09-04 23:53:04 -0300

    aaaa

--- lib/Pages/Game/Util/Decks/Demacia.dart
+++ lib/Pages/Game/Util/Decks/Demacia.dart
@@ -1,1 +1,52 @@
+import 'package:pac4/Pages/Game/Card/GameCardClass.dart';
 
+final deckDemacia = <GameCardClass>[
+  GameCardClass(
+    region: Region.DEMACIA,
+    name: 'garen',
+    rarity: Rarity.LEGENDARY,
+    atributtes: Attributes(1, 1, 1, 1),
+  ),
+  GameCardClass(
+    region: Region.DEMACIA,
+    name: 'cavaleiro',
+    rarity: Rarity.COMMON,
+    atributtes: Attributes(1, 1, 1, 1),
+  ),
+  GameCardClass(
+    region: Region.DEMACIA,
+    name: 'duelista',
+    rarity: Rarity.COMMON,
+    atributtes: Attributes(1, 1, 1, 1),
+  ),
+  GameCardClass(
+    region: Region.DEMACIA,
+    name: 'ferreiro',
+    rarity: Rarity.COMMON,
+    atributtes: Attributes(1, 1, 1, 1),
+  ),
+  GameCardClass(
+    region: Region.DEMACIA,
+    name: 'mago_real',
+    rarity: Rarity.COMMON,
+    atributtes: Attributes(1, 1, 1, 1),
+  ),
+  GameCardClass(
+    region: Region.DEMACIA,
+    name: 'novata',
+    rarity: Rarity.COMMON,
+    atributtes: Attributes(1, 1, 1, 1),
+  ),
+  GameCardClass(
+    region: Region.DEMACIA,
+    name: 'poro_guerrero',
+    rarity: Rarity.COMMON,
+    atributtes: Attributes(1, 1, 1, 1),
+  ),
+  GameCardClass(
+    region: Region.DEMACIA,
+    name: 'valquiria',
+    rarity: Rarity.COMMON,
+    atributtes: Attributes(1, 1, 1, 1),
+  ),
+];
